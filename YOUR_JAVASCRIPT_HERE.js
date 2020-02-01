0b479d27363a94f1d095cf0fc7af1a96eceef757
nickland42 <nickland421@gmail.com>
2020-02-01 16:39:10 +0100

    worked on some general styling

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -4,27 +4,20 @@ const myHero = {
 };
 var hero = {
   name:"Nick", 
-  heroic: {  },
+  heroic: {},
   inventory: [ ],
   health: 10,
   // weapon: (weaponType),
   
 };
+// console.log(hero.name)
 
 const teastCreature2 = { health: 2}
 const teastCreature4 = { health:4 }
 
 function rest(person) {
-
-  
-
-
-
-  
   window.alert("Your health is already at 10, FIGHT ON!.")
-}
-
-
+};
 
 function pickUpItem(params) {
   
@@ -33,3 +26,14 @@ function equipWeapon(params) {
   
 };
 
+// function nameFunction (){
+//   document.getElementById(heroName).value = "your Hero's name is:" + hero.name;
+// }
+
+document.getElementById("clickMe").onclick = doFunction;
+document.getElementById("clickMe").onclick = function () { alert('hello!'); };
+var el = document.getElementById("clickMe");
+if (el.addEventListener)
+    el.addEventListener("click", doFunction, false);
+else if (el.attachEvent)
+    el.attachEvent('onclick', doFunction);
