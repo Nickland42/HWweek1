b51983eb7cb40dc36491a0938a0e1587b8c763a6
nickland42 <nickland421@gmail.com>
2020-02-01 12:35:37 +0100

    images addes & functions setup

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,7 +1,6 @@
 // Write your JS here
 const myHero = {
 
-
 };
 var hero = {
   name:"Nick", 
@@ -12,4 +11,24 @@ var hero = {
   
 };
 
+const teastCreature2 = { health: 2}
+const teastCreature4 = { health:4 }
+
+function rest(person) {
+  return testCreature = {},
+  
+
+
+  
+  window.alert("Your health is already at 10, FIGHT ON!.")
+}
+
+
+
+function pickUpItem(params) {
+  
+};
+function equipWeapon(params) {
+  
+};
 
